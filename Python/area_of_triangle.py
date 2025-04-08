6331a8fe005655f548a764c830dd36382d4c4e0d
Gokul-28 <87302750+Gokul-28@users.noreply.github.com>
2025-04-08 07:54:09 +0530

    First Python Project added

---
+++ Python/area_of_triangle.py
@@ -0,0 +1,9 @@
+# Area of Triangle formula is 1/2 * Base * Height
+
+width = float(input("What is the width of the area: "))
+height = float(input("What is the height of the area: "))
+
+# Calculation 
+area = 0.5 * width * height
+
+print(f'The area of right triangle is {area}')
